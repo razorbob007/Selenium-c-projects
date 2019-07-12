8156b8f8b9da9e04e7023e740cc33881638aecee
razorbob007 <ebobula1@gmail.com>
2019-07-12 15:18:58 +0200

    renamed folder, added step definitions

--- MyStore/Extensions/WebDriverExtensions.cs
+++ MyStore/Extensions/WebDriverExtensions.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MyStore.Extentions
+namespace MyStore.Extensions
 {
     public static class WebDriverExtensions
     {
--- MyStore/Steps/Driver_Step_Definitions.cs
+++ MyStore/Steps/Driver_Step_Definitions.cs
@@ -1,12 +1,48 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TechTalk.SpecFlow;
 
 namespace MyStore.Steps
 {
-    public sealed class Driver_Step_Definitions
+    public static class Driver_Step_Definitions
     {
+        public static ChromeDriver driver;
+        public static IWebDriver Driver { get; set; }
+
+        [BeforeTestRun]
+        public static void Initialize()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--start-maximized");
+            driver = new ChromeDriver(options);
+            driver.Manage().Cookies.DeleteAllCookies();
+        }
+        public static ChromeDriver ChromeDriverInstance
+        {
+            get { return driver; }
+        }
+
+
+        [TearDown]
+        public static void TearDown()
+        {
+            driver.Close();
+            driver.Quit();
+            driver.Dispose();
+        }
+
+        [AfterTestRun]
+        public static void AfterMethod()
+        {
+            driver.Close();
+            driver.Quit();
+            driver.Dispose();
+        }
     }
 }
